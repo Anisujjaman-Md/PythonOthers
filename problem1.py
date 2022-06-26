4426271d5e66dfa84a726d1c94c95ac4a408e79a
Md-Anisujjaman <contact.anisujjaman@gmail.com>
2022-06-27 05:24:45 +0600

    problem1 updated

--- problem1.py
+++ problem1.py
@@ -1,10 +1,14 @@
 # What 3 Number Addition is 13 [12, 1, 8, 5, 6 , 4, 5] 
 
-num = [12, 1, 8, 5, 6 , 4, 5]
+num = [12, 3, 8, 5, 6 , 4, 5]
 
-for item in num:
-    for item_ in num:
-        for item__ in num:
-            if item != item_ and item != item__ and item_ != item__ and item + item_ + item__ == 13:
-                print(item, item_, item__)
-                break
+def main():  
+    for item in num:
+        for item_ in num:
+            for item__ in num:
+                if item != item_ and item != item__ and item_ != item__ and item + item_ + item__ == 13:
+                    # print(item, item_, item__)
+                    # break
+                    return print(item, item_, item__)
+
+main()
